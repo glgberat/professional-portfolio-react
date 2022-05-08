c590af77feba05e2d0f88f289d1d0457b6fb3e21
Abbush <glgberat@yahoo.com>
2022-05-08 17:24:29 -0400

    Nav Footer Components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div class="footer" id="footer">
+        <a href="https://github.com/glgberat"><img src="https://img.icons8.com/material-outlined/48/000000/github.png" alt="Github" class="icon"/></a>
+        <a href="https://www.linkedin.com/in/mehmet-huyuk-0a822888"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
+        <a href="https://twitter.com/glgbtr"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" class="icon"/></a>
+        
+    </div>
+    )
+}
+
+export default Footer;
