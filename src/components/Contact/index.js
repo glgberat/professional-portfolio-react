e57ddf69cec71e4c19bd5a205a0453568d29482e
Abbush <glgberat@yahoo.com>
2022-05-08 22:44:41 -0400

    bug fixing and deploying

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -43,15 +43,15 @@ return (
         <form class="justify-content-center" id="contact-form">
             <div>
                 <label htmlFor="name">Name:</label>
-                <input class="form-control" type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
+                <input className="form-control" type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
             </div>
             <div >
                 <label htmlFor="email">Email:</label>
-                <input class="form-control" type="email"  name="email" defaultValue={email} onBlur={handleChange} />
+                <input className="form-control" type="email"  name="email" defaultValue={email} onBlur={handleChange} />
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea class="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="7" />
+                <textarea className="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="7" />
             </div> 
             {errorMessage && (
             <div>
@@ -60,7 +60,7 @@ return (
             )}
 
             <div>
-            <button data-testid='button' class="btn btn-outline-dark mt-4" type="submit" onSubmit={handleSubmit}>Submit</button>
+            <button data-testid='button' className="btn btn-outline-dark mt-4" type="submit" onSubmit={handleSubmit}>Submit</button>
             </div>
         </form>
     </section>
