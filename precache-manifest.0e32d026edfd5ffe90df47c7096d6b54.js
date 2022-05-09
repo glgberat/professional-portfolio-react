5894c4f1f03535f3726f60c63a77f3dc0992146b
Abbush <glgberat@yahoo.com>
2022-05-08 22:53:25 -0400

    Updates

---
+++ precache-manifest.0e32d026edfd5ffe90df47c7096d6b54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c0f3c093f31f350fb264f01eb0d2d8",
+    "url": "/professional-portfolio-react/index.html"
+  },
+  {
+    "revision": "ed9711c135b218c1daa6",
+    "url": "/professional-portfolio-react/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "f1bff5bc9713a77ff830",
+    "url": "/professional-portfolio-react/static/css/main.9390337e.chunk.css"
+  },
+  {
+    "revision": "ed9711c135b218c1daa6",
+    "url": "/professional-portfolio-react/static/js/2.e8bbc613.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/professional-portfolio-react/static/js/2.e8bbc613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332eb526f2f960fc972a",
+    "url": "/professional-portfolio-react/static/js/3.0ced90f8.chunk.js"
+  },
+  {
+    "revision": "f1bff5bc9713a77ff830",
+    "url": "/professional-portfolio-react/static/js/main.b1a35d7c.chunk.js"
+  },
+  {
+    "revision": "2cf05c14536a595b8797",
+    "url": "/professional-portfolio-react/static/js/runtime-main.5c4e7d4a.js"
+  }
+]);
